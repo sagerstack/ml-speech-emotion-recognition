1d98e5628a2851be0b8d12e45d0625271b6da1d5
sagerstack <sagarpratapsingh15@gmail.com>
2025-11-26 23:28:21 +0800

    fixed tf formats

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -44,12 +44,12 @@ module "vpc" {
   public_subnets  = [for index in range(length(local.azs)) : cidrsubnet(var.vpc_cidr, 8, index)]
   private_subnets = [for index in range(length(local.azs)) : cidrsubnet(var.vpc_cidr, 8, index + 10)]
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_dns_hostnames   = true
-  enable_dns_support     = true
-  public_subnet_tags     = { "kubernetes.io/role/elb" = 1 }
-  private_subnet_tags    = { "kubernetes.io/role/internal-elb" = 1 }
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  public_subnet_tags      = { "kubernetes.io/role/elb" = 1 }
+  private_subnet_tags     = { "kubernetes.io/role/internal-elb" = 1 }
   map_public_ip_on_launch = true
 
   tags = local.tags
@@ -202,12 +202,12 @@ resource "aws_iam_role_policy" "github_actions" {
 }
 
 data "aws_eks_cluster" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
